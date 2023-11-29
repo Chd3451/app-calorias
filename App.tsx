84f32a5cc4fc953f801c9a41ba3575cda90bf6b1
Charlie Delgado <charliedelgado12@icloud.com>
2023-11-28 21:46:23 -0400

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{flex: 1}}>
+        
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+
